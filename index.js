ca32515f1c3f3a24fe864f5f6c6c646d1d632ddc
wildanzr <graita.sukma@gmail.com>
2023-04-01 16:32:24 +0700

    feat: safe in local"

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
 const nodemailer = require('nodemailer')
 const hbs = require('nodemailer-express-handlebars')
 const path = require('path')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
+const dotenv = require('dotenv')
+
+// Load env
+dotenv.config()
+
+console.log(process.env.OAUTH_EMAIL)
+
+const oauth2Client = new OAuth2(
+  process.env.OAUTH_CLIENT_ID,
+  process.env.OAUTH_CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+)
+
+// Set Credentials
+oauth2Client.setCredentials({
+  refresh_token: process.env.OAUTH_REFRESH_TOKEN
+})
+
+const accessToken = oauth2Client.getAccessToken()
 
 // Create nodemailer config
 const configOptions = {
-  host: 'smtp.gmail.com',
-  port: 587,
+  service: 'gmail',
   tls: {
-    rejectUnauthorized: false,
-    minVersion: 'TLSv1.2'
+    rejectUnauthorized: false
   },
   auth: {
-    user: 'wildanz.reyz@gmail.com',
-    pass: 'mmthretwwujrgapy'
+    type: 'OAuth2',
+    user: process.env.OAUTH_EMAIL,
+    accessToken,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
   }
 }
 
 // Create nodemailer transporter
 const transporter = nodemailer.createTransport(configOptions)
-transporter.use('compile', hbs({
-  viewEngine: {
-    extName: '.hbs',
-    partialsDir: path.resolve(__dirname, './views'),
-    defaultLayout: false
-  },
-  viewPath: path.join(__dirname, './views'),
-  extName: '.handlebars'
-}))
+transporter.use(
+  'compile',
+  hbs({
+    viewEngine: {
+      extName: '.hbs',
+      partialsDir: path.resolve(__dirname, './views'),
+      defaultLayout: false
+    },
+    viewPath: path.join(__dirname, './views'),
+    extName: '.handlebars'
+  })
+)
 
 // Create nodemailer mail options
 const mailOptions = {
-  from: 'wildanz.reyz@gmail.com',
-  to: 'damodaradamiono665@gmail.com',
-  subject: 'Hello from nodemailer',
+  from: process.env.OAUTH_EMAIL,
+  to: 'graita.sukma@gmail.com',
+  subject: 'Aktivasi Akun LetsCode',
   text: 'Hello from nodemailer',
   template: 'register',
   context: {
     name: 'Miawwwww',
-    link: 'https://letscode.wildanzr.my.id/auth/activate?token=d6be7604a331d2df8ca3e157e655d59ec9180f07'
+    link: 'https://letscode.wildanzr.my.id/auth/activate?token=d6be7604a331d2df8ca3e157e655d59ec9180f07',
+    alternative: 'https://letscode.wildanzr.my.id/auth/activate?token=d6be7604a331d2df8ca3e157e655d59ec9180f07'
   }
 }
 
